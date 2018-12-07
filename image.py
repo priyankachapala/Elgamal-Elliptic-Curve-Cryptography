afe60fdd7ef1fdfeda9d7356d78e05e62ac2b699
priyankachapala <lakshmipryanka.chapala@gmail.com>
2018-12-07 15:30:50 +0530

    image encryption and decryption

---
+++ image.py
@@ -0,0 +1,208 @@
+# Basics of Elliptic Curve Cryptography implementation on Python
+import collections
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+# for black and white image
+img1 = cv2.imread('cam.tif')
+# for color images
+img1 = cv2.mpimg('lenaclor.ppm')
+[h,w,c]=img1.shape
+img2 = np.zeros((h, w, c),np.int32)
+img3 = np.zeros((h, w, c),np.int32)
+def inv(n, q):
+"""div on PN modulo a/b mod q as a * inv(b, q) mod q
+>>> assert n * inv(n, q) % q == 1
+"""
+for i in range(q):
+if (n * i) % q == 1:
+return i
+pass
+assert False, "unreached"
+pass
+def sqrt(n, q):
+"""sqrt on PN modulo: returns two numbers or exception if not exist
+>>> assert (sqrt(n, q)[0] ** 2) % q == n
+>>> assert (sqrt(n, q)[1] ** 2) % q == n
+"""
+assert n < q
+for i in range(1, q):
+if (i * i) % q == n:
+return (i, q - i)
+pass
+raise Exception("not found")
+Coord = collections.namedtuple("Coord", ["x", "y"])
+6
+class EC(object):
+"""System of Elliptic Curve"""
+def __init__(self, a, b, q):
+"""elliptic curve as: (y**2 = x**3 + a * x + b) mod q
+- a, b: params of curve formula
+- q: prime number
+"""
+assert 0 < a and a < q and 0 < b and b < q and q > 2
+assert (4 * (a ** 3) + 27 * (b ** 2)) % q != 0
+self.a = a
+self.b = b
+self.q = q
+# just as unique ZERO value representation for "add": (not on curve)
+self.zero = Coord(0, 0)
+pass
+def is_valid(self, p):
+if p == self.zero:
+return True
+l = (p.y ** 2) % self.q
+r = ((p.x ** 3) + self.a * p.x + self.b) % self.q
+return l == r
+def at(self, x):
+"""find points on curve at x
+- x: int < q
+- returns: ((x, y), (x,-y)) or not found exception
+>>> a, ma = ec.at(x)
+>>> assert a.x == ma.x and a.x == x
+>>> assert a.x == ma.x and a.x == x
+>>> assert ec.neg(a) == ma
+>>> assert ec.is_valid(a) and ec.is_valid(ma)
+"""
+assert x < self.q
+ysq = (x ** 3 + self.a * x + self.b) % self.q
+y, my = sqrt(ysq, self.q)
+return Coord(x, y), Coord(x, my)
+def neg(self, p):
+"""negate p
+>>> assert ec.is_valid(ec.neg(p))
+"""
+7
+return Coord(p.x, -p.y % self.q)
+def add(self, p1, p2):
+"""<add> of elliptic curve: negate of 3rd cross point of (p1,p2) line
+>>> d = ec.add(a, b)
+>>> assert ec.is_valid(d)
+>>> assert ec.add(d, ec.neg(b)) == a
+>>> assert ec.add(a, ec.neg(a)) == ec.zero
+>>> assert ec.add(a, b) == ec.add(b, a)
+>>> assert ec.add(a, ec.add(b, c)) == ec.add(ec.add(a, b), c)
+"""
+if p1 == self.zero:
+return p2
+if p2 == self.zero:
+return p1
+if p1.x == p2.x and (p1.y != p2.y or p1.y == 0):
+# p1 + -p1 == 0
+return self.zero
+if p1.x == p2.x:
+# p1 + p1: use tangent line of p1 as (p1,p1) line
+l = (3 * p1.x * p1.x + self.a) * inv(2 * p1.y, self.q) % self.q
+pass
+else:
+l = (p2.y - p1.y) * inv(p2.x - p1.x, self.q) % self.q
+pass
+x = (l * l - p1.x - p2.x) % self.q
+y = (l * (p1.x - x) - p1.y) % self.q
+return Coord(x, y)
+def mul(self, p, n):
+"""n times <mul> of elliptic curve
+>>> m = ec.mul(p, n)
+>>> assert ec.is_valid(m)
+>>> assert ec.mul(p, 0) == ec.zero
+"""
+r = self.zero
+m2 = p
+# O(log2(n)) add
+while 0 < n:
+if n & 1 == 1:
+r = self.add(r, m2)
+pass
+n, m2 = n >> 1, self.add(m2, m2)
+pass
+# [ref] O(n) add
+# for i in range(n):
+8
+# r = self.add(r, p)
+# pass
+return r
+def order(self, g):
+"""order of point g
+>>> o = ec.order(g)
+>>> assert ec.is_valid(a) and ec.mul(a, o) == ec.zero
+>>> assert o <= ec.q
+"""
+assert self.is_valid(g) and g != self.zero
+for i in range(1, self.q + 1):
+if self.mul(g, i) == self.zero:
+return i
+pass
+raise Exception("Invalid order")
+pass
+if __name__ == "__main__":
+# shared elliptic curve system of examples
+a = int(input("enter curve parameter 'a': "))
+b = int(input("enter curve parameter 'b': "))
+q = int(input("enter prime number 'q' (prime number): "))
+ec = EC(a, b, q)
+na = int(input("enter private key of A:"))
+nb = int(input("enter private key of B:"))
+k = int(input("enter a random number: "))
+# produce generator point
+g, m = ec.at(0)
+#assert ec.order(g) <= ec.q
+print("Generator Point",g)
+#public keys
+pa = ec.mul(g, na)
+pb = ec.mul(g, nb)
+print("public Key of A",pa)
+print("public Key of B",pb)
+kg = ec.mul(g, k)
+#Creating Dictionary for pixel intensities
+Dict={}
+Dict[0]=ec.mul(g,1)
+#Creating Dictionary for image intensties
+9
+for i in range(1,256):
+Dict[i]=ec.mul(g,(i))
+print(Dict)
+#image encryption Process Starts
+for i in range(0,h):
+for j in range(0,w):
+img2[i,j,0]=ec.add(Dict[img1[i,j,0]],ec.mul(pb,k)).x
+for i in range(0,h):
+for j in range(0,w):
+img2[i,j,1]=ec.add(Dict[img1[i,j,1]],ec.mul(pb,k)).x
+for i in range(0,h):
+for j in range(0,w):
+img2[i,j,2]=ec.add(Dict[img1[i,j,2]],ec.mul(pb,k)).x
+#Decryption starts
+for i in range(0,h):
+for j in range(0,w):
+dec,enc=ec.at(img2[i,j,0])
+m=ec.add(dec,ec.neg(ec.mul(kg,nb)))
+n=ec.add(enc,ec.neg(ec.mul(kg,nb)))
+for key in Dict.keys():
+if Dict[key]==m or Dict[key]==n:
+img3[i,j,0]=key
+for i in range(0,h):
+for j in range(0,w):
+dec,enc=ec.at(img2[i,j,1])
+m=ec.add(dec,ec.neg(ec.mul(kg,nb)))
+n=ec.add(enc,ec.neg(ec.mul(kg,nb)))
+for key in Dict.keys():
+if Dict[key]==m or Dict[key]==n:
+img3[i,j,1]=key
+for i in range(0,h):
+for j in range(0,w):
+dec,enc=ec.at(img2[i,j,2])
+m=ec.add(dec,ec.neg(ec.mul(kg,nb)))
+n=ec.add(enc,ec.neg(ec.mul(kg,nb)))
+for key in Dict.keys():
+if Dict[key]==m or Dict[key]==n:
+img3[i,j,2]=key
+10
+#plotiing encypted and Decrypted images
+plt.imshow(img1,interpolation='nearest')
+plt.show()
+plt.imshow(img2, interpolation='nearest')
+plt.show()
+plt.imshow(img3, interpolation='nearest')
+plt.show()
+pass
